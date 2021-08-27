72c5893f54bf02886b3dd974a71dac4bd2c2fd43
SungJaeCho <liadus@naver.com>
2021-08-28 08:28:56 +0900

    별출력 2

---
+++ src/main/java/_17_star2/Main.java
@@ -0,0 +1,26 @@
+package _17_star2;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+            *****
+             ****
+              ***
+               **
+                *
+        */
+        int n = 8;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                if(i>j){
+                    System.out.print(' ');
+                } else {
+                    System.out.print('*');
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
