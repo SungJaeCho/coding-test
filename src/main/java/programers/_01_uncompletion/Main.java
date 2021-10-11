743281987c395eb97148ab51f204e7dbed9a039f
SungJaeCho <liadus@naver.com>
2021-10-11 09:10:34 +0900

    프로그래머스 완주하지 못한 선수

--- src/main/java/programers/_01_uncompletion/Main.java
+++ src/main/java/programers/_01_uncompletion/Main.java
@@ -1,5 +1,7 @@
 package programers._01_uncompletion;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         String[] participant = {"mislav", "stanko", "mislav", "ana"};
@@ -9,26 +11,40 @@ public class Main {
 
     public static String solution(String[] participant, String[] completion) {
         String answer = "";
-        boolean check;
-        for(int i=0; i<participant.length; i++) {
-            check = false;
-            for(int j=0; j<completion.length; j++) {
-                if(participant[i].equals(completion[j])) {
-                    participant[i] = "";
-                    completion[j] = "";
-                    check = true;
-                    continue;
-                }
-            }
-            if(!check) {
-                answer = participant[i];
+
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        int i;
+        for(i=0; i<completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                return participant[i];
             }
         }
+
+        return participant[i];
+
+
+
+
+//        for(int i=0; i<participant.length; i++) {
+//            check = false;
+//            for(int j=0; j<completion.length; j++) {
+//                if(participant[i].equals(completion[j])) {
+//                    participant[i] = "";
+//                    completion[j] = "";
+//                    check = true;
+//                    continue;
+//                }
+//            }
+//            if(!check) {
+//                answer = participant[i];
+//            }
+//        }
 
 //        for(int i=0; i< participant.length; i++) {
 //            answer += participant[i];
 //        }
 
-        return answer;
+//        return answer;
     }
 }
