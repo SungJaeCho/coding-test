001face074321a48a1db98954c1d2a465f4c3c45
sungjaecho <liadus@naver.com>
2022-01-27 18:06:30 +0900

    백준_2606 dfs

---
+++ src/main/java/_39_123plus/Main.java
@@ -0,0 +1,30 @@
+package _39_123plus;
+/*
+정수 4를 1, 2, 3의 합으로 나타내는 방법은 총 7가지가 있다. 합을 나타낼 때는 수를 1개 이상 사용해야 한다.
+1+1+1+1
+1+1+2
+1+2+1
+2+1+1
+2+2
+1+3
+3+1
+정수 n이 주어졌을 때, n을 1, 2, 3의 합으로 나타내는 방법의 수를 구하는 프로그램을 작성하시오.
+예제입력:
+4
+7
+10
+출력
+7
+44
+274
+문제 출처: 백준 https://www.acmicpc.net/problem/9095
+*/
+
+public class Main {
+    static int[] cache = new int[1000];
+
+    public static void main(String[] args) {
+
+
+    }
+}
