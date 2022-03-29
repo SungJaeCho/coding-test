ce8b94e36dc0af59373ffa825a523aa899b1efd5
sungjaecho <liadus@naver.com>
2022-03-29 14:30:28 +0900

    theKWeakestRowsInAMatrix1337 clear

--- src/main/java/binarySearch/theKWeakestRowsInAMatrix1337.java
+++ src/main/java/binarySearch/theKWeakestRowsInAMatrix1337.java
@@ -5,14 +5,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class theKWeakestRowsInAMatrix1337 {
+    static List<Integer> list = new ArrayList<>();
+    static List<Integer> result = new ArrayList<>();
+
     public static void main(String[] args) {
-        int[][] mat = {{1,1,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,0,0,0},{1,1,1,1,1}};
-        int k = 3;
+//        int[][] mat = {{1,1,0,0,0},{1,1,1,1,0},{1,0,0,0,0},{1,1,0,0,0},{1,1,1,1,1}};
+//        int k = 3;
+        int[][] mat = {{1,0,0,0},{1,1,1,1},{1,0,0,0},{1,0,0,0}};
+        int k = 2;
         System.out.println(Arrays.toString(kWeakestRows(mat,k)));
     }
 
     public static int[] kWeakestRows(int[][] mat, int k) {
         int[] arr = new int[mat.length];
+        list.clear();
+        result.clear();
 
         for(int i=0; i<mat.length; i++) {
             int s = 0;
@@ -22,13 +29,38 @@ public class theKWeakestRowsInAMatrix1337 {
             arr[i] = s;
         }
         System.out.println(Arrays.toString(arr));
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-        int[] result = new int[k];
-        for(int i=0; i<k; i++) {
-            result[i] = arr[i];
+        for (int i = 0; i<arr.length; i++) {
+            sort(arr[i], i);
         }
-        return result;
+        System.out.println(result);
+
+        int[] resultArr = new int[k];
+        for (int i=0; i<k; i++) {
+            resultArr[i] = result.get(i);
+        }
+        return resultArr;
+    }
+
+    public static void sort(int n, int index) {
+        boolean chk = false;
+
+        if(list.isEmpty()) {
+            list.add(n);
+            result.add(0);
+        } else {
+            for(int i=0; i<list.size(); i++) {
+                if(n < list.get(i)) {
+                    list.add(i,n);
+                    result.add(i,index);
+                    chk = true;
+                    break;
+                }
+            }
+            if(!chk) {
+                list.add(n);
+                result.add(list.size()-1);
+            }
+        }
     }
 
 }
