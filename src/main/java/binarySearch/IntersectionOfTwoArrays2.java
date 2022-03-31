ea15b95ae9625ebac8d6440e59bfc3596e88dde0
sungjaecho <liadus@naver.com>
2022-03-31 11:10:40 +0900

    350. Intersection of Two Arrays II clear

--- src/main/java/binarySearch/IntersectionOfTwoArrays2.java
+++ src/main/java/binarySearch/IntersectionOfTwoArrays2.java
@@ -4,31 +4,27 @@ import java.util.*;
 
 public class IntersectionOfTwoArrays2 {
     public static void main(String[] args) {
-        int[] num1 = {2,1};
-        int[] num2 = {1,2};
+//        int[] num1 = {2,2};
+//        int[] num2 = {2,2};
+//        int[] num1 = {1,2,2,1};
+//        int[] num2 = {2};
+        int[] num1 = {61,24,20,58,95,53,17,32,45,85,70,20,83,62,35,89,5,95,12,86,58,77,30,64,46,13,5,92,67,40,20,38,31,18,89,85,7,30,67,34,62,35,47,98,3,41,53,26,66,40,54,44,57,46,70,60,4,63,82,42,65,59,17,98,29,72,1,96,82,66,98,6,92,31,43,81,88,60,10,55,66,82,0,79,11,81};
+        int[] num2 = {5,25,4,39,57,49,93,79,7,8,49,89,2,7,73,88,45,15,34,92,84,38,85,34,16,6,99,0,2,36,68,52,73,50,77,44,61,48};
         System.out.println(Arrays.toString(intersect(num1, num2)));
     }
 
     public static int[] intersect(int[] nums1, int[] nums2) {
         int low = 0, high;
         List<Integer> list = new ArrayList<>();
-        if(nums1.length >= nums2.length) {
-            high = nums1.length -1;
-            Arrays.sort(nums1);
-            for(int target : nums2) {
-                int result = search(nums1, target, low, high);
-                if(result >= 0) {
-                    list.add(result);
-                }
-            }
-        } else {
-            high = nums2.length -1;
+
+        for(int target : nums1) {
             Arrays.sort(nums2);
-            for(int target : nums1) {
-                int result = search(nums2, target, low, high);
-                if(result >= 0) {
-                    list.add(result);
-                }
+            high = nums2.length -1;
+            int result = search(nums2, target, low, high);
+            System.out.println(result);
+            if(result >= 0) {
+                list.add(target);
+                nums2[result] = -1;
             }
         }
         return list.stream().mapToInt(k -> k).toArray();
@@ -38,7 +34,7 @@ public class IntersectionOfTwoArrays2 {
         while(low <= high) {
             int mid = (low+high)/2;
             if(target == arr[mid]) {
-                return target;
+                return mid;
             } else if(target > arr[mid]) {
                 low = mid + 1;
             } else {
