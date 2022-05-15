ae038315b0ed664ca18405c1ea5c1710b39d7698
sungjaecho <liadus@naver.com>
2022-05-16 07:52:13 +0900

    fibo 수정

--- src/main/java/_34_fibonacci/Main.java
+++ src/main/java/_34_fibonacci/Main.java
@@ -12,10 +12,11 @@ public class Main {
 
     }
     public static int fibo(int n) {
-        if (n == 1 || n == 2) {
-            return 1;
+        if(n <= 1) {
+            return n;
+        } else {
+            return fibo(n-1) + fibo(n-2);
         }
-        return fibo(n-1) + fibo(n-2);
-
     }
+
 }
